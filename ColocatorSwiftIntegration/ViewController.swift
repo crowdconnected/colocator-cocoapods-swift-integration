d54caf4cd7d9b80b3c55a2e6daa8e7dfc8c282d3
Robert Tanase <robert@crowdconnected.com>
2019-12-04 11:43:10 +0000

    Initial Commit

---
+++ ColocatorSwiftIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColocatorSwiftIntegration
+//
+//  Created by TCode on 04/12/2019.
+//  Copyright © 2019 CrowdConnected. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
